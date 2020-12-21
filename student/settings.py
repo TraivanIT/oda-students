c519ad9a8a0c4d8b140c3453550e6052844753e2
Traivan <traivanit@gmail.com>
2020-12-21 07:17:41 +0000

    add all from python anywhere

--- student/settings.py
+++ student/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rr^u682w+0(&6cr9k2xh@qa_m5uyemj(a1%69b(iv&8@kh*r-*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://traivan.pythonanywhere.com]
 
 
 # Application definition
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
